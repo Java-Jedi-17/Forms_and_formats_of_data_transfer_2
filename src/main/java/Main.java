2ffcbcb384e5658c2ce3e2019d1d5f958ffdfd8d
JavaJedi <aleksk170400@gmail.com>
2024-09-25 02:06:28 +0500

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Server server = new Server(64);
+        server.start(9999);
+    }
 }
